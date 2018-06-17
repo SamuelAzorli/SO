8202f5cab0984a1cfe1864cda38c508790da0f87
Samuel Lahr Azorli <37658907+SamuelAzorli@users.noreply.github.com>
2018-06-17 14:21:01 -0300

    Add files via upload
    Adicionado o Input via linha de comando

--- SOprojeto.c
+++ SOprojeto.c
@@ -2,6 +2,7 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 typedef struct lnha
 {
@@ -36,18 +37,80 @@ double valor;
 int inicializar(char *nome);
 void *buscar(void *num);
 
-int main()
+int main(int argc, char *argv[])
 {
 
     FILE *arq;
-    char nome[50];
+    char *nome;
+    clock_t start, end;
     long l;//variavel de loop
     unsigned int erro=0;
-    int tam;
+    //int tam;
     int erro2;//para detectar erro
     achados *percorre;
+    switch(argc)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        printf("Argumentos insuficientes!/n");
+        printf("Uso: <numero de linhas> <numero de colunas> <numero de threads> <valor a ser buscado> <nome do arquivo>\n\a");
+        break;
+    case 6:
+        nome=argv[5];
+        linhas=atoi(argv[1]);
+        colunas=atoi(argv[2]);
+        quanthreads=atoi(argv[3]);
+        valor=atof(argv[4]);
+        erro2=inicializar(nome);
+        if(erro2==1)
+        {
+            printf("Nao foi possivel abrir o arquivo ou nome do arquivo invalido \a\n");
+            pthread_exit(0);
+        }
 
-    printf("Insira o nome do arquivo:");
+        pthread_t *thds;
+        thds=(pthread_t *)malloc(quanthreads*sizeof(pthread_t));
+        start=clock();
+        for(l=0;l<quanthreads;l++)
+        {
+            printf("Criando a thread numero: %ld\n", l);
+            erro=pthread_create(&thds[l], NULL, buscar, (void *) l);
+            if(erro)
+            {
+                printf("Nao foi possivel criar a %ld thread\n", l+1);
+                exit(-1);//está no tutorial em ingles que foi passado
+            }
+        }
+
+        for(l=0;l<quanthreads; l++)
+        {
+            pthread_join(thds[l], NULL);//espera todas as threads terminarem
+        }
+        end=clock();
+        printf("Busca terminada, tempo gasto: %lf s\n\n",(double)(end-start)/(double)(CLOCKS_PER_SEC));
+        percorre=aini;
+        if(aini==NULL)
+        {
+            printf("Nao existe o valor na matriz!\a\n");
+        }
+        else
+        while(percorre!=NULL)
+        {
+            printf("Linha: %d, Coluna: %d; \n", percorre->linha, percorre->coluna);
+            percorre=percorre->prox;
+        }
+        break;
+    default:
+        printf("Muitos parametros!/n");
+        printf("Uso: <numero de linhas> <numero de colunas> <numero de threads> <valor a ser buscado> <nome do arquivo>\n\a");
+        break;
+    }
+
+    /*printf("Insira o nome do arquivo:");
     fgets(nome, 50, stdin);
     tam=strlen(nome);
     nome[tam-1]='\0';
@@ -58,44 +121,7 @@ int main()
     printf("Insira a quantidade de colunas:");
     scanf("%d", &colunas);
     printf("Insira o valor a ser buscado:");
-    scanf("%lf", &valor);
-
-    erro2=inicializar(nome);
-    if(erro2==1)
-    {
-        printf("Nao foi possivel abrir o arquivo ou nome do arquivo invalido \a\n");
-        pthread_exit(0);
-    }
-
-    pthread_t *thds;
-    thds=(pthread_t *)malloc(quanthreads*sizeof(pthread_t));
-    for(l=0;l<quanthreads;l++)
-    {
-        printf("Criando a thread numero: %ld\n", l);
-        erro=pthread_create(&thds[l], NULL, buscar, (void *) l);
-        if(erro)
-        {
-            printf("Nao foi possivel criar a %ld thread\n", l+1);
-            exit(-1);//está no tutorial em ingles que foi passado
-        }
-    }
-
-    for(l=0;l<quanthreads; l++)
-    {
-        pthread_join(thds[l], NULL);//espera todas as threads terminarem
-    }
-
-    percorre=aini;
-    if(aini==NULL)
-    {
-        printf("Nao existe o valor na matriz!\a\n");
-    }
-    else
-    while(percorre!=NULL)
-    {
-        printf("Linha: %d, Coluna: %d; \n", percorre->linha, percorre->coluna);
-        percorre=percorre->prox;
-    }
+    scanf("%lf", &valor);*/
 
     pthread_exit(0);
 }
@@ -138,8 +164,7 @@ int inicializar(char *nome)
 
             novo=(lin*)malloc(sizeof(lin));
             fscanf(arq," %lf ", &novo->dado);
-            //printf("Shwi ");
-            printf("%lf ", novo->dado);
+            //printf("%lf ", novo->dado);
             novo->linha=l;
             novo->coluna=c;
             novo->prox=NULL;
@@ -158,7 +183,7 @@ int inicializar(char *nome)
             }
         }
         fscanf(arq," %c", &temp);//ler o '\n'
-        printf("\n");
+        //printf("\n");
     }
     fclose(arq);
     return 0;
@@ -167,7 +192,6 @@ int inicializar(char *nome)
 void *buscar(void *num)
 {
     long id= (long) num;
-    printf("Eu sou a thread numero: %ld\n", id);
     col *percorre;
     percorre=inicio;
     int nlinha=1;
