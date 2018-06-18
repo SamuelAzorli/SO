7005490001b6386b1a8f444e82a4de9750ac7079
Samuel Lahr Azorli <37658907+SamuelAzorli@users.noreply.github.com>
2018-06-18 17:20:37 -0300

    Pequenas Mudanças
    Modificado os nomes das variáveis e pequenas correções de erros

--- SOprojeto.c
+++ SOprojeto.c
@@ -4,51 +4,60 @@
 #include<string.h>
 #include<time.h>
 
-typedef struct lnha
+typedef struct s_linha
 {
     double dado;
     int linha, coluna;//para identificar o número da linha e coluna;
-    struct lnha *prox;//como o acesso só ocorre em uma direção, não precisa usar ponteiro para cima, baixo e esquerda
-}lin;
+    struct s_linha *prox;//como o acesso só ocorre em uma direção, não precisa usar ponteiro para cima, baixo e esquerda
+}t_linha;
 
-typedef struct coluna
+typedef struct s_indice
 {
 /*Esta estrutura é formada por vários nós cada um deles contendo o início de uma linha*/
-    lin *inicio;
-    struct coluna *prox;
-}col;
+    t_linha *inicio;
+    struct s_indice *prox;
+}t_indice;
 
-typedef struct acho
+typedef struct s_encontrou
 {
 /*essa estrutura guarda as linhas e colunas em que o valor foi encontrado*/
     int linha;
     int coluna;
-    struct acho *prox;
-}achados;
+    struct s_encontrou *prox;
+}t_encontrou;
 
 
-col *inicio;
-achados *aini;
-int quanthreads;
-int linhas;
-int colunas;
-double valor;
+t_indice *ti_inicio;//inicio do tipo indice
+t_encontrou *te_inicio;//inicio do tipo encontrou
+int quanthreads;//quantidade de threads
+int linhas;//quantidade de linhas
+int colunas;//quantidade de colunas
+double valor;//valor que será buscado
+//pthread_mutex_t mutex;//semáforo
 
+/*========escopo das funções========*/
 int inicializar(char *nome);
 void *buscar(void *num);
+/*==================================*/
 
 int main(int argc, char *argv[])
 {
-
-    FILE *arq;
-    char *nome;
+    char *nome;//para armazenar o nome do arquivo
     clock_t start, end;
-    long l;//variavel de loop
-    unsigned int erro=0;
-    //int tam;
-    int erro2;//para detectar erro
-    achados *percorre;
+    /*variáveis para medir o tempo, encontrei no seguinte site:
+    https://www.geeksforgeeks.org/how-to-measure-time-taken-by-a-program-in-c/*/
+    long int l;//variavel de loop
+    unsigned int erro1=0;
+    int erro2;//erro1 e erro2 para detectar erro usando o retorno das funções
+    t_encontrou *percorre;//auxiliar para percorrer a lista encadeada
+
+    void *status;
+    pthread_attr_t atributos;
+    pthread_attr_init(&atributos);
+    pthread_attr_setdetachstate(&atributos, PTHREAD_CREATE_JOINABLE);
+
     switch(argc)
+    //trata os inputs da linha de comando
     {
     case 0:
     case 1:
@@ -65,35 +74,41 @@ int main(int argc, char *argv[])
         colunas=atoi(argv[2]);
         quanthreads=atoi(argv[3]);
         valor=atof(argv[4]);
-        erro2=inicializar(nome);
+        erro2=inicializar(nome);//inicializa o programa carregando os dados do arquivo na memória
         if(erro2==1)
         {
             printf("Nao foi possivel abrir o arquivo ou nome do arquivo invalido \a\n");
             pthread_exit(0);
         }
 
-        pthread_t *thds;
-        thds=(pthread_t *)malloc(quanthreads*sizeof(pthread_t));
-        start=clock();
+        //alocação do vetor das threads:
+        pthread_t *threads;
+        threads=(pthread_t *)malloc(quanthreads*sizeof(pthread_t));
+
+        start=clock();//começa a contar o tempo
         for(l=0;l<quanthreads;l++)
+        //loop de criação das threads
         {
-            printf("Criando a thread numero: %ld\n", l);
-            erro=pthread_create(&thds[l], NULL, buscar, (void *) l);
-            if(erro)
+            printf("Criando a thread numero: %ld\n", l+1);
+            erro1=pthread_create(&threads[l], &atributos, buscar, (void *) (l+1));//as threads executam a função "buscar"
+            if(erro1)//tratamento de erro do pthread_create
             {
                 printf("Nao foi possivel criar a %ld thread\n", l+1);
-                exit(-1);//está no tutorial em ingles que foi passado
+                exit(-1);
             }
         }
 
         for(l=0;l<quanthreads; l++)
         {
-            pthread_join(thds[l], NULL);//espera todas as threads terminarem
+            pthread_join(threads[l], &status);
+            //espera todas as threads terminarem
         }
-        end=clock();
+        end=clock();//termina a contagem de tempo
         printf("Busca terminada, tempo gasto: %lf s\n\n",(double)(end-start)/(double)(CLOCKS_PER_SEC));
-        percorre=aini;
-        if(aini==NULL)
+        pthread_attr_destroy(&atributos);
+
+        percorre=te_inicio;
+        if(te_inicio==NULL)
         {
             printf("Nao existe o valor na matriz!\a\n");
         }
@@ -110,76 +125,68 @@ int main(int argc, char *argv[])
         break;
     }
 
-    /*printf("Insira o nome do arquivo:");
-    fgets(nome, 50, stdin);
-    tam=strlen(nome);
-    nome[tam-1]='\0';
-    printf("Insira a quantidade de threads:");
-    scanf("%d", &quanthreads);
-    printf("Insira a quantidade de linhas:");
-    scanf("%d", &linhas);
-    printf("Insira a quantidade de colunas:");
-    scanf("%d", &colunas);
-    printf("Insira o valor a ser buscado:");
-    scanf("%lf", &valor);*/
-
     pthread_exit(0);
 }
 
 int inicializar(char *nome)
 {
     FILE *arq;
-    arq=fopen(nome, "r");
-    aini=NULL;
-    if(arq==NULL)
+    arq=fopen(nome, "r");//abre o arquivo
+    te_inicio=NULL;
+    if(arq==NULL)//trata erro ao abrir o arquivo
     {
         return 1;
     }
-    int c;
-    int l;
-    lin *novo, *aux;
-    col *percorre, *colnovo;
-    char temp;//isso foi para resolver o warning do fscanf para ler o "\n"
+    int c;//variavel de loop para alocação da linha
+    int l;//variavel de loop para alocação do indice
+    t_linha *novo, *tl_percorre;
+    t_indice *ti_percorre, *ti_novo;
+    char temp;//variável para receber o '/n' do fscanf
 
     for(l=1; l<=linhas; l++)
+    //esse loop aloca o tipo indice
     {
-        colnovo=(col*)malloc(sizeof(col));
-        colnovo->inicio=NULL;
-        colnovo->prox=NULL;
-        if(inicio==NULL)
+        ti_novo=(t_indice*)malloc(sizeof(t_indice));
+        ti_novo->inicio=NULL;
+        ti_novo->prox=NULL;
+        if(ti_inicio==NULL)
         {
-            inicio=colnovo;
+            ti_inicio=ti_novo;
         }
         else
         {
-            percorre=inicio;
-            while(percorre->prox!=NULL)
+            ti_percorre=ti_inicio;
+            while(ti_percorre->prox!=NULL)
+            /*esse loop coloca o ponteiro na ultima posição, para inserir o novo nó no fim,
+            isso deixa o código menos eficiente em termos de desempenho mas mantém a ordem
+            da matriz na memória igual a que está no arquivo, sem isso teria que inserir o
+            novo nó no inicio*/
             {
-                percorre=percorre->prox;
+                ti_percorre=ti_percorre->prox;
             }
-            percorre->prox=colnovo;
+            ti_percorre->prox=ti_novo;
         }
         for(c=1; c<=colunas; c++)
+        //loop para alocar a linha
         {
-
-            novo=(lin*)malloc(sizeof(lin));
+            novo=(t_linha*)malloc(sizeof(t_linha));
             fscanf(arq," %lf ", &novo->dado);
             //printf("%lf ", novo->dado);
             novo->linha=l;
             novo->coluna=c;
             novo->prox=NULL;
-            if(colnovo->inicio==NULL)
+            if(ti_novo->inicio==NULL)
             {
-                colnovo->inicio=novo;
+                ti_novo->inicio=novo;
             }
             else
             {
-                aux=colnovo->inicio;
-                while(aux->prox!=NULL)
+                tl_percorre=ti_novo->inicio;
+                while(tl_percorre->prox!=NULL)
                 {
-                    aux=aux->prox;
+                    tl_percorre=tl_percorre->prox;
                 }
-                aux->prox=novo;
+                tl_percorre->prox=novo;
             }
         }
         fscanf(arq," %c", &temp);//ler o '\n'
@@ -191,49 +198,49 @@ int inicializar(char *nome)
 /*====================================================================================*/
 void *buscar(void *num)
 {
-    long id= (long) num;
-    col *percorre;
-    percorre=inicio;
-    int nlinha=1;
-    lin *aux;
-    achados *achx;
-    achados *novo;
+    long id= (long) num;//numero para identificar a thread (é diferente do thread_id)
+    t_indice *ti_percorre;
+    ti_percorre=ti_inicio;
+    int nlinha=1;//isso é para identificar qual é a proxima linha que a thread irá executar
+    t_linha *tl_percorre;
+    t_encontrou *te_percorre;
+    t_encontrou *novo;
     int proxlinha=id;//indica qual será a proxima linha que a thread lerá
 
-    while(percorre!=NULL)
+    while(ti_percorre!=NULL)
     {
         if(nlinha==proxlinha)
         {
-            aux=percorre->inicio;
-            while(aux!=NULL)//percorre dentro da linha
+            tl_percorre=ti_percorre->inicio;
+            while(tl_percorre!=NULL)//percorre dentro da linha
             {
-                if(aux->dado==valor)
+                if(tl_percorre->dado==valor)
                 {
-                   novo=(achados*)malloc(sizeof(achados));
-                    novo->linha=aux->linha;
-                    novo->coluna=aux->coluna;
+                   novo=(t_encontrou*)malloc(sizeof(t_encontrou));
+                    novo->linha=tl_percorre->linha;
+                    novo->coluna=tl_percorre->coluna;
                     novo->prox=NULL;
-                    if(aini==NULL)
+                    if(te_inicio==NULL)
                     {
-                        aini=novo;
+                        te_inicio=novo;
                     }
                     else
                     {
-                        achx=novo;
-                        while(achx->prox!=NULL)
+                        te_percorre=novo;
+                        while(te_percorre->prox!=NULL)
                         {
-                            achx=achx->prox;
+                            te_percorre=te_percorre->prox;
                         }
-                    achx->prox=novo;
+                    te_percorre->prox=novo;
                     }
                 }
 
-                aux=aux->prox;
+                tl_percorre=tl_percorre->prox;
             }
-            proxlinha+=id;
+            proxlinha+=quanthreads;
         }
         nlinha++;
-        percorre=percorre->prox;
+        ti_percorre=ti_percorre->prox;
     }
     pthread_exit(num);
 }
